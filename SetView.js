b0adbb14059b832f76a2ef55ac11b9c83613bba5
Nick Strayer <nick@nicks-mbp.attlocal.net>
2017-05-07 18:54:50 -0500

    Sets and auto filling weights are in'
    :

--- SetView.js
+++ SetView.js
@@ -22,21 +22,32 @@ class SetView extends Component{
 
         //construct an array of objects for each set we're doing.
         const liftData = this.props.liftData;
-        const setsInfo = this.makeSetsArray(liftData,"");
+        let setsInfo = this.makeSetsArray(liftData,"");
 
         var dataSource = new ListView.DataSource(
             {rowHasChanged: (r1,r2) => r1.weight !== r2.weight}
         );
 
         this.state = {
+            oneRepMax: "0",
+            setsInfo: setsInfo,
             ds: dataSource,
             dataSource: dataSource.cloneWithRows(setsInfo),
             liftName: this.props.liftName,
         }
     }
 
+    calculateWeight(weight, percentage){
+        let setWeight = 0;
+        if(!isNaN(weight) && !isNaN(percentage)){
+            const rawWeight = weight * (percentage/100);
+            setWeight = Math.floor(rawWeight/5)*5;
+        }
+        return setWeight.toString();
+    }
+
     makeSetsArray(liftData, weight){
-        const setsInfo = [];
+        let setsInfo = [];
 
         for(let i = 0; i < liftData.sets; i++){
             setsInfo.push(
@@ -44,23 +55,47 @@ class SetView extends Component{
                     setNum: i + 1,
                     reps: liftData.reps,
                     percentage: liftData.percentage,
-                    weight:this.calculateWeight(weight, liftData.percentage)
+                    weight: this.calculateWeight(weight, this.props.liftData.percentage),
+                    userChanged: false, //know if the user has changed this value so we to update it or not when changing 1rm later.
                 }
             );
         }
         return(setsInfo)
     }
 
-    onTextChanged(text) {
-
+    newSetWeight(inputWeight,setNum){
+        let newSetInfo = this.state.setsInfo
+        newSetInfo[setNum - 1].weight = inputWeight;
+        newSetInfo[setNum - 1].userChanged = true;
+        this.updateSetsInfo(newSetInfo);
+        // console.log(newSetInfo)
     }
-    calculateWeight(weight, percentage){
-        let setWeight = 0;
-        if(!isNaN(weight) && !isNaN(percentage)){
-            const rawWeight = weight * (percentage/100);
-            setWeight = Math.floor(rawWeight/5)*5;
-        }
-        return setWeight;
+
+    newRepNum(inputReps,setNum){
+        let newSetInfo = this.state.setsInfo
+        newSetInfo[setNum - 1].reps = inputReps;
+        newSetInfo[setNum - 1].userChanged = true;
+        this.updateSetsInfo(newSetInfo);
+        // console.log(newSetInfo)
+    }
+
+    updateOneRepMax(newMax){
+        this.setState({oneRepMax: newMax});
+        let newSetInfo = this.state.setsInfo;
+        newSetInfo.forEach(set => set.weight = this.calculateWeight(newMax, this.props.liftData.percentage))
+        this.updateSetsInfo(newSetInfo);
+    }
+
+    //updates the set info state variable and also the set's given rows.
+    updateSetsInfo(newSetInfo){
+        this.setState({
+            setsInfo: newSetInfo,
+            dataSource: this.state.ds.cloneWithRows(newSetInfo)
+        });
+    }
+
+    saveToDropbox(){
+        console.log(this.state.setsInfo);
     }
 
     renderSet(setInfo) {
@@ -77,56 +112,58 @@ class SetView extends Component{
                             Set {setNum}
                         </Text>
                     </View>
-                    <View style = {styles.setInfo}>
-                        <Text style={styles.infoText}>
-                            {repNum} reps
-                        </Text>
+                    <View style={styles.reps}>
+                        <TextInput
+                            style={styles.repInput}
+                            keyboardType = 'numeric'
+                            placeholder= {`${repNum}`}
+                            onChangeText = {(text)=> this.newRepNum(text, setNum)}
+                        />
+                        <Text style = {styles.poundValue}> reps </Text>
                     </View>
-                    <View style = {styles.setInfo}>
-                        <Text style={styles.infoText}>
-                            {weight} lbs
-                        </Text>
+                    <View style={styles.oneRepMax}>
+                        <TextInput
+                            style={styles.weightInput}
+                            keyboardType = 'numeric'
+                            placeholder= {`${weight}`}
+                            onChangeText = {(text)=> this.newSetWeight(text, setNum)}
+                        />
+                        <Text style = {styles.poundValue}> lbs </Text>
                     </View>
-                    <TextInput
-                        style = {styles.repInput}
-                        keyboardType = 'numeric'
-                        placeholder = "# reps gotten"
-                        onChangeText = {(text)=> console.log(text)}
-                    />
                 </View>
                 <View style={styles.separator}/>
             </View>
-    );
-    }
-
-    updateWeight(text){
-        //construct an array of objects for each set we're doing.
-        const liftData = this.props.liftData;
-        const setsInfo = this.makeSetsArray(liftData,text);
-
-        this.setState({dataSource: this.state.ds.cloneWithRows(setsInfo)})
-        console.log(setsInfo)
+        );
     }
 
     render(){
-        // console.log(this.state);
         return(
             <View style = {styles.pageContainer}>
                 <View style={styles.header}>
                     <Text style={styles.title}>
                         {this.state.liftName}
                     </Text>
-                    <TextInput
-                        style={styles.repInput}
-                        keyboardType = 'numeric'
-                        placeholder= "1 rep max"
-                        onChangeText = {(text) => this.updateWeight(text)}
-                    />
+                    <View style={styles.oneRepMax}>
+                        <TextInput
+                            style={styles.oneRepMaxInput}
+                            keyboardType = 'numeric'
+                            placeholder= "1 rep max"
+                            onChangeText = {(text) => this.updateOneRepMax(text)}
+                        />
+                        <Text style = {styles.poundValue}> lbs </Text>
+                    </View>
                 </View>
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={this.renderSet.bind(this)}
                 />
+                <View style = {styles.setRow}>
+                  <TouchableHighlight style = {styles.saveButton}
+                              underlayColor='orangered'
+                              onPress={this.saveToDropbox.bind(this)} >
+                              <Text style = {styles.buttonText}>Save</Text>
+                  </TouchableHighlight>
+                </View>
             </View>
         );
     }
@@ -149,13 +186,31 @@ var styles = StyleSheet.create({
     setInfo:{
         flex:1
     },
-    repInput:{
+    oneRepMax: {
+        flex:1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    oneRepMaxInput:{
         textAlign: "right",
         fontSize: 18,
         color: '#656565',
-        marginRight: 5,
         fontSize: 18,
-        flex:1,
+        flex:5,
+    },
+    poundValue: {
+        textAlign: "left",
+        fontSize: 17,
+        color: '#656565',
+        flex: 1
+    },
+    weightInput:{
+        textAlign: "right",
+        fontSize: 18,
+        color: '#656565',
+        fontSize: 18,
+        flex:2,
     },
     infoText:{
         fontSize: 20,
@@ -166,10 +221,40 @@ var styles = StyleSheet.create({
         backgroundColor: '#ccebc5',
         padding:10,
     },
+    reps: {
+        flex:1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    repInput:{
+        textAlign: "right",
+        fontSize: 18,
+        color: '#656565',
+        fontSize: 18,
+        flex:1,
+    },
     title:{
         fontSize: 30,
         color: '#656565'
-    }
+    },
+    buttonText: {
+        fontSize: 18,
+        color: 'white',
+        alignSelf: 'center'
+    },
+    saveButton: {
+        height: 36,
+        flex: 1,
+        flexDirection: 'row',
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderWidth: 1,
+        borderRadius: 8,
+        // marginBottom: 10,
+        alignSelf: 'stretch',
+        justifyContent: 'center'
+    },
 });
 
 module.exports = SetView;
